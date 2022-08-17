14f51b4a4c85076dad8b06a2f143e7aed9ed6e37
Lawrence Lim <ll4715@nyu.edu>
2022-08-17 15:34:50 +0800

    Made small changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Smart Contract Lottery
+
+The contract deployment ID is:
+```
+0xB2Bf3C63C8aFD537a76fD346Ab88a24B6C76aAf5
+```
--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -1,5 +1,5 @@
 const { assert, expect } = require('chai')
-const { getNamedAccounts, deployments, ethers, network } = require('hardhat')
+const { getNamedAccounts, ethers } = require('hardhat')
 const {
   developmentChains,
   networkConfig,
