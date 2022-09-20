bd83311ab028c389c3cde941ae68c944039266c5
Lawrence Lim <ll4715@nyu.edu>
2022-09-20 17:01:33 +0800

    Updated .env values

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,11 @@ require('hardhat-gas-reporter')
 require('hardhat-contract-sizer')
 require('dotenv').config()
 
-// const RINKEBY_RPC_URL =
-//   process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby/example'
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby/example'
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '0x756c10f614795972a3d269eefdea3ee32d557065297f08bd6d0a7f30db04eebd'
-// const COINMKTCAP_API_KEY = process.env.COINMKTCAP_API_KEY
-// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMKTCAP_API_KEY = process.env.COINMKTCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -28,15 +28,15 @@ module.exports = {
     // },
   },
   solidity: '0.8.9',
-  // etherscan: {
-  //   apiKey: ETHERSCAN_API_KEY,
-  // },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   gasReporter: {
     enabled: false,
     currency: 'USD',
     outputFile: 'gas-report.txt',
     noColors: false,
-    // coinmarketcap: COINMKTCAP_API_KEY,
+    coinmarketcap: COINMKTCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
